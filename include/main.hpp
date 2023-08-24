7f876e5a4ea7b1131eba68627436bcf5010b8e2d
MiBeloz <miha_bmv_@mail.ru>
2023-08-24 16:28:48 +0600

    triangle

--- include/main.hpp
+++ include/main.hpp
@@ -5,14 +5,13 @@
 #include <iostream>
 
 
-//extern "C" { // Чтобы выбиралась не "Intel(R) HD Graphics", а "NVidia GeForce" (или какая там у вас видеокарта)
+// extern "C" { // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ "Intel(R) HD Graphics", пїЅ "NVidia GeForce" (пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ)
 //    _declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
-//    // Для NVidia
-//       // __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1; // Это для AMD, но не проверял.
-//}
+//    // пїЅпїЅпїЅ NVidia
+//       // __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1; // пїЅпїЅпїЅ пїЅпїЅпїЅ AMD, пїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
+// }
 
-int gWindowSizeX = 640;
-int gWindowSizeY = 480;
+
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow, const int widgh, const int height);
 void glfwKeyCallback(GLFWwindow* pWindow, const int key, const int scancode, const int action, const int mode);
